b607a1a3c8f1f7ddf1657ea7cc737893f79d6c6a
Yeison <yeismopu@gmail.com>
2026-02-20 14:15:03 -0500

    Actualización workflow Terraform CI/CD multi-entorn  - permiso owner

--- keyvault.tf
+++ keyvault.tf
@@ -1,5 +1,5 @@
 resource "azurerm_key_vault" "kv" {
-  name                			= "kv-reto-pragma-yeismopu"
+  name                			= "kv-reto-pragma-${var.environment}"
   location            			= azurerm_resource_group.rg.location
   resource_group_name 			= azurerm_resource_group.rg.name
   tenant_id           			= data.azurerm_client_config.current.tenant_id
