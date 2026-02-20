bb9ad6b2cef80c990ab2c6146756e1169ee85122
Yeison <yeismopu@gmail.com>
2026-02-20 14:40:08 -0500

    Initial commit - Terraform infraestructura ecommerce - backend

--- backend.tf
+++ backend.tf
@@ -3,6 +3,6 @@ terraform {
     resource_group_name   = "RG-IaC"
     storage_account_name  = "tfstateyeismopu"
     container_name        = "tfstate"
-    key                   = "pruebareto.tfstate"
+    key                   = "placeholder.tfstate"
   }
 }
